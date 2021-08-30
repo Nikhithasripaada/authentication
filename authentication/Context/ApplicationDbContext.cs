c21531290cdaa7903c7f425116df733738b6e090
Quadrant <Quadrant@DESKTOP-PKA345O>
2021-08-30 11:13:21 +0530

    Add project files.

---
+++ authentication/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using authentication.authenticate;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace authentication.Context
+{
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+            }
+            protected override void OnModelCreating(ModelBuilder builder)
+            {
+                base.OnModelCreating(builder);
+            }
+        }
+ 
+}
+
